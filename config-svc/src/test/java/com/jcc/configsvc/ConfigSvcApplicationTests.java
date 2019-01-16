5f4c1ec66e861d175ccab81b3368739bd9debb87
kamel NAJJAR <kamel@kamels-MacBook-Pro.local>
2019-01-16 07:56:37 +0100

    change pc

---
+++ config-svc/src/test/java/com/jcc/configsvc/ConfigSvcApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jcc.configsvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigSvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
